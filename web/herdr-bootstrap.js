63530e8465e88e1de960a7f4c3032167a64754a3
Demetre Minchev <mitko19@gmail.com>
2026-09-25 10:10:34 +1000

    release: web bundle for v1.0.3

--- web/herdr-bootstrap.js
+++ web/herdr-bootstrap.js
@@ -1,4 +1,4 @@
-const e = new URL(window.__HERDR_ENTRY__ || "/builds/1.0.2-387-ab222b69c6cff83c/index.html", location);
+const e = new URL(window.__HERDR_ENTRY__ || "/builds/1.0.3-387-de7e50c2b28e1d1b/index.html", location);
   e.search = location.search;
   e.hash = location.hash;
   location.replace(e);
